db4a58bae273827c709917ceb2d50f16f77054fc
akshatkareer1011 <49092873+akshatkareer1011@users.noreply.github.com>
2025-03-26 22:25:11 +0530

    Update common.js

--- public/js/common.js
+++ public/js/common.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://localhost:5000";
+const API_BASE_URL = "vbuy-web-app-production.up.railway.app";
 
 function getTokenFromCookies() {
   let cookies = document.cookie.split("; ");
