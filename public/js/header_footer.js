f307ca90c97fdaf44cf3bb5a58fb939ff0318a36
Akshat Kareer <akshat.kareer@gmail.com>
2025-04-07 22:21:28 +0530

    Made website responsive

--- public/js/header_footer.js
+++ public/js/header_footer.js
@@ -36,7 +36,45 @@ window.addEventListener("DOMContentLoaded", function () {
         }
       });
     }
+    const logoutBtn = document
+      .querySelector(".nav-logout-btn a")
+      .addEventListener("click", async e => {
+        e.preventDefault();
+
+        const token = getTokenFromCookies();
+
+        try {
+          const response = await fetch(`${API_BASE_URL}/user/logout`, {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${token}`,
+            },
+          });
+
+          if (!response.ok) {
+            alert("Something went wrong!");
+            throw new Error("Error");
+          }
+
+          window.location.href = "/login";
+        } catch (err) {
+          console.error(err);
+        }
+      });
+
+    const headerElement = document.querySelector(".header");
+    const btnElement = document.querySelector(".show-hide-btns");
+
+    btnElement.addEventListener("click", e => {
+      headerElement.classList.toggle("nav-open");
+    });
+
+    this.setTimeout(() => {
+      document.querySelector(".navbar").style.transition = "all 0.5s ease-in";
+    }, 500);
   });
+
   loadComponent("footer-container", "footer.html");
 });
 
