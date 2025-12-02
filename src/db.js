320dd89c78f94f7285cf71c443aede3ea62565d7
Akshat Kareer <49092873+akshatkareer1011@users.noreply.github.com>
2025-12-02 15:18:47 +0530

    New Database setup

--- src/db.js
+++ src/db.js
@@ -8,7 +8,7 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  port: 3306,
+  port: 16005,
   waitForConnections: true,
   connectionLimit: 5,
   queueLimit: 0,
